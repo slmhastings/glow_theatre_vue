eea9053210b819779cf2569f15bb73a13f88e500
Sequona Hastings <slmhastings@gmail.com>
2020-08-19 19:18:43 -0500

    initial commit

--- src/views/UsersNew.vue
+++ src/views/UsersNew.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="userNew">
     <h1>{{ message }}</h1>
   </div>
 </template>
@@ -11,7 +11,7 @@
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to New User Page!",
     };
   },
   created: function () {},
